39556c0212bf099a16c6a49daaa6962e769fe8c0
dyu <david.yu.ftw@gmail.com>
2016-08-22 19:06:44 +0800

    constants

--- lib/gen/common.d.ts
+++ lib/gen/common.d.ts
@@ -1,3 +1,41 @@
+export declare const c: {
+    PREVENT_NONE: number;
+    PREVENT_DEFAULT: number;
+    PREVENT_PROPAGATION: number;
+    PREVENT_BOTH: number;
+    STATE_NONE: number;
+    STATE_SUCCESS: number;
+    STATE_ERROR: number;
+    STATE_WARNING: number;
+    STATE_LOADING: number;
+    MASK_STATUS: number;
+    STATE_UPDATE: number;
+    LSTATE_NONE: number;
+    LSTATE_INCLUDED: number;
+    LSTATE_SELECTED: number;
+    LSTATE_REFRESH: number;
+    MASK_SELECTED_REFRESH: number;
+    STATE_DESC: number;
+    STATE_LOAD_NEWER: number;
+    STATE_LOAD_OLDER: number;
+    STATE_RELOAD: number;
+    MASK_RPC: number;
+    STATE_LOCAL_SEARCH: number;
+    MASK_RPC_DISABLE: number;
+    HANDLE_ACTIVATE: number;
+    HANDLE_PASSIVATE: number;
+    HANDLE_NEW: number;
+    HANDLE_UPDATE: number;
+    HANDLE_DELETE: number;
+    HANDLE_ENTRY_INPUT: number;
+    HANDLE_ENTRY_DELETE: number;
+    HANDLE_ENTRY_SORT: number;
+    HANDLE_ENTRY_SEARCH: number;
+    SELECT_CLICKED: number;
+    SELECT_REFRESH: number;
+    SELECT_CLICKED_UPDATE: number;
+    SELECT_FORCE: number;
+};
 export declare const default_obj: {};
 /**
  * The arg: value can be number or string.
