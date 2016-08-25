591954823bdf3edfd308fad7d9c03c8c80db60f6
dyu <david.yu.ftw@gmail.com>
2016-08-25 15:27:48 +0800

    pclass default prefix

--- src/d/pclass.ts
+++ src/d/pclass.ts
@@ -1,13 +1,15 @@
 import { addClass, removeClass } from '../util'
 
-export function bind() {}
+export function bind() {
+    this.prefix = this.arg || ''
+}
 
 export function update(value: any, oldValue: any) {
     if (oldValue) {
-        removeClass(this.el, this.arg + oldValue)
+        removeClass(this.el, this.prefix + oldValue)
     }
     if (value) {
-        addClass(this.el, this.arg + value)
+        addClass(this.el, this.prefix + value)
         oldValue = value
     }
 }
