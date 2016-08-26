09a92562edccd8736dbf997714fa3a2e022ccf24
dyu <david.yu.ftw@gmail.com>
2016-08-26 23:28:13 +0800

    pclass

---
+++ src/d2/pclass.ts
@@ -0,0 +1,19 @@
+import { VNode, VNodeDirective, VNodeWithData } from '../v2/'
+import { addClass, removeClass } from '../util'
+
+export function bind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {}
+
+export function update(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    let prefix = dir.arg || '',
+        value = dir.value,
+        oldValue = dir.oldValue
+    
+    if (oldValue) {
+        removeClass(el, prefix + oldValue)
+    }
+    if (value) {
+        addClass(el, prefix + value)
+    }
+}
+
+export function unbind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {}
