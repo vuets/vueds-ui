a158b005b476c922d465d95567bb3cd96d57cbc6
dyu <david.yu.ftw@gmail.com>
2016-09-01 16:33:08 +0800

    generated files

--- lib/vm_util.d.ts
+++ lib/vm_util.d.ts
@@ -1,3 +1,4 @@
+export declare function getFirstVm(el: any): any;
 export declare function getValue(obj: any, key: string): any;
 export declare function vmGetValue(obj: any, key: string): any;
 export declare function vmLookup(target: any, p?: any): any;
