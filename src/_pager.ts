228bf2be675b249c7758151d30ada05cf2c1691e
dyu <david.yu.ftw@gmail.com>
2016-09-01 21:22:26 +0800

    move to _pager

---
+++ src/_pager.ts
@@ -0,0 +1,231 @@
+// common dependency for d/pager and d2/pager
+
+import * as keymage from './keymage'
+
+import {
+    screen, table_compact_columns, 
+} from './screen_util'
+
+import { isInput, resolveElement, fireEvent } from './dom_util'
+
+import {
+    selectIdx, pageAndSelectIdx, 
+    listUp, listDown, 
+    tableUp, tableDown, tableLeft, tableRight, tableJumpUp, tableJumpDown, tableJumpLeft, tableJumpRight,
+    moveTopOrUp, moveBottomOrDown, moveLeft, moveRight,
+    pageFirst, pageLast, pageSort, pageReload,
+    pageNextOrLoad, pagePrevOrLoad
+} from './pager_util'
+
+import { vfragLookup } from './vm_util'
+
+import {
+    Pager, PagerState, PojoStore, PojoState, SelectionFlags, SelectionType, resolveNextPageIndex 
+} from 'vueds/lib/store/'
+
+export interface Opts {
+    flags: number
+    col_size: number
+    table_flags: number
+
+    loop_var: string
+    hammer: any,
+    pager: Pager
+    vm: any
+}
+
+var current: Opts
+
+export function moveUp(e) {
+    var target = e.target,
+        attr = target.getAttribute('c-list'),
+        pager,
+        clickUpdate: boolean
+    
+    if (attr && !(current = target.pager_opts)) {
+        target.pager_opts = current = resolveElement(target, attr).pager_opts
+    }
+    
+    if (!current || !(pager = current.pager).index_hidden) return
+    
+    clickUpdate = !!(current.flags & screen.flags)
+    if (current.col_size)
+        tableUp(pager, current.col_size, current.table_flags, pager.index_selected, e, clickUpdate)
+    else
+        listUp(pager, pager.index_selected, e, clickUpdate)
+}
+
+export function moveDown(e) {
+    var target = e.target,
+        attr = target.getAttribute('c-list'),
+        pager: Pager,
+        clickUpdate: boolean
+    
+    if (attr && !(current = target.pager_opts)) {
+        target.pager_opts = current = resolveElement(target, attr).pager_opts
+    }
+    
+    if (!current || !(pager = current.pager).index_hidden) return
+    
+    clickUpdate = !!(current.flags & screen.flags)
+    if (current.col_size)
+        tableDown(pager, current.col_size, current.table_flags, pager.index_selected, e, clickUpdate)
+    else
+        listDown(pager, pager.index_selected, e, clickUpdate)
+}
+
+export function focus(e, opts: Opts) {
+    current = opts
+    keymage.setScope('pager')
+}
+
+export function swipe(e, opts: Opts) {
+    var target = e.target,
+        vfrag,
+        vm
+    if (isInput(target)) return
+    current = opts
+    keymage.setScope('pager')
+    
+    vfrag = vfragLookup(target)
+    if (!vfrag || !(vm = vfrag.scope) || !vm.hasOwnProperty('$index') || 
+        (vm !== opts.vm && vm.$parent !== opts.vm)) return
+    
+    switch(e.direction) {
+        case 2: // right-to-left
+            pageNextOrLoad(e, opts.pager, opts)
+            break
+        case 4: // left-to-right
+            pagePrevOrLoad(e, opts.pager, opts)
+            break;
+    }
+}
+
+export function select(e, opts: Opts, dbltap: boolean, flagsIntersect: number) {
+    var target = e.target, 
+        parent = target.parentElement, 
+        vfrag = vfragLookup(target, parent),
+        pojo,
+        store: PojoStore<any>,
+        vm,
+        trigger = target.getAttribute('dtap') || (target=parent).getAttribute('dtap')
+    
+    if (trigger) fireEvent(target, 'dtap')
+    
+    if (!vfrag || !(vm = vfrag.scope) || !vm.hasOwnProperty('$index') || 
+        (vm !== opts.vm && vm.$parent !== opts.vm) || !(pojo = vm[opts.loop_var])) return
+    
+    store = opts.pager['store']
+
+    if (flagsIntersect) {
+        if (!(pojo.vstate & PojoState.UPDATE)) {
+            pojo.vstate |= PojoState.UPDATE
+            store.select(pojo, SelectionFlags.CLICKED_UPDATE, vm.$index)
+        }
+    } else if (dbltap && trigger) {
+        store.select(pojo, SelectionFlags.CLICKED, vm.$index)
+    } else {
+        pojo.vstate ^= PojoState.UPDATE
+        store.select(pojo, SelectionFlags.CLICKED_UPDATE, vm.$index)
+    }
+}
+
+export function press(e, opts: Opts) {
+    current = opts
+    if (isInput(e.target)) return
+    keymage.setScope('pager')
+    
+    select(e, opts, false, opts.flags & screen.flags)
+}
+
+export function tap(e, opts: Opts) {
+    current = opts
+    if (isInput(e.target)) return
+    keymage.setScope('pager')
+    
+    if (opts.flags & screen.flags) {
+        select(e, opts, false, 1)
+        return
+    }
+
+    var vfrag = vfragLookup(e.target),
+        pojo,
+        store: PojoStore<any>,
+        vm
+    if (!vfrag || !(vm = vfrag.scope) || !vm.hasOwnProperty('$index') || 
+        (vm !== opts.vm && vm.$parent !== opts.vm) || !(pojo = vm[opts.loop_var])) return
+    
+    store = opts.pager['store']
+    store.select(pojo, SelectionFlags.CLICKED, vm.$index)
+}
+
+export function doubletap(e, opts: Opts) {
+    // TODO remove hack
+    if (e.target.hasOwnProperty('$l')) {
+        // a date input with a date picker
+        // workaround for mobile
+        fireEvent(e.target, 'dblclick')
+        e.preventDefault()
+        //e.stopPropagation()
+        return false
+    }
+    
+    current = opts
+    if (isInput(e.target)) return
+    // tap already handles this, which gets called before this function
+    // keymage.setScope('pager')
+    
+    select(e, opts, true, opts.flags & screen.flags)
+}
+
+/**
+ * ```{loop_var}__{flags}__{table_flags?}```
+ */
+export function putArgsTo(opts: Opts, split_args: string[]) {
+    let i = 0, len = split_args.length
+    
+    opts.loop_var = split_args[i++]
+    opts.flags = parseInt(split_args[i++], 10)
+    
+    if (i === len) return
+    opts.table_flags = parseInt(split_args[i++], 10)
+    opts.col_size = table_compact_columns()
+}
+
+export function addCustomListenersTo(el, opts: Opts) {
+    let pageLeft = (e) => { current = opts; pagePrevOrLoad(e, opts.pager, opts); },
+        pageRight = (e) => { current = opts; pageNextOrLoad(e, opts.pager, opts); }
+    
+    el.addEventListener('page-left', pageLeft)
+    el.addEventListener('page-right', pageRight)
+
+    opts['unbind'] = () => {
+        el.removeEventListener('page-left', pageLeft)
+        el.removeEventListener('page-right', pageRight)
+    }
+}
+
+export function configureHammer(hammer: any, opts: Opts) {
+    hammer.get('swipe').set({ velocity: 0.1/*, distance: 1*/ })
+    hammer.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) )
+    hammer.get('doubletap').recognizeWith('tap')
+
+    hammer.on('swipe', (e) => swipe(e, opts))
+    hammer.on('press', (e) => press(e, opts))
+    hammer.on('tap', (e) => tap(e, opts))
+    hammer.on('doubletap', (e) => doubletap(e, opts))
+}
+
+// =====================================
+// key bindings
+
+keymage.$('pager', 'up', moveUp)
+keymage.$('pager', 'down', moveDown)
+keymage.$('pager', 'defmod-up', (e) => { if (current) moveTopOrUp(e, current.pager, current) })
+keymage.$('pager', 'defmod-down', (e) => { if (current) moveBottomOrDown(e, current.pager, current) })
+keymage.$('pager', ['left', 'shift-up'], (e) => { if (current) moveLeft(e, current.pager, current) })
+keymage.$('pager', ['right', 'shift-down'], (e) => { if (current) moveRight(e, current.pager, current) })
+keymage.$('pager', ['defmod-left', 'defmod-shift-up'], (e) => { if (current) pageFirst(e, current.pager, current) })
+keymage.$('pager', ['defmod-right', 'defmod-shift-down'], (e) => { if (current) pageLast(e, current.pager, current) })
+keymage.$('pager', 'shift-space', (e) => { if (current) pageSort(e, current.pager, current) })
+keymage.$('pager', 'defmod-space', (e) => { if (current) pageReload(e, current.pager, current) })
--- src/d/pager.ts
+++ src/d/pager.ts
@@ -1,214 +1,9 @@
 declare function require(path: string): any;
 
-import {
-    screen, table_compact_columns, 
-} from '../screen_util'
+import { focus, putArgsTo, addCustomListenersTo, configureHammer } from '../_pager'
 
-import { isInput, resolveElement, fireEvent } from '../dom_util'
+const Hammer = require('hammerjs')
 
-import {
-    selectIdx, pageAndSelectIdx, 
-    listUp, listDown, 
-    tableUp, tableDown, tableLeft, tableRight, tableJumpUp, tableJumpDown, tableJumpLeft, tableJumpRight,
-    moveTopOrUp, moveBottomOrDown, moveLeft, moveRight,
-    pageFirst, pageLast, pageSort, pageReload,
-    pageNextOrLoad, pagePrevOrLoad
-} from '../pager_util'
-
-import { vfragLookup } from '../vm_util'
-
-import {
-    Pager, PagerState, PojoStore, PojoState, SelectionFlags, SelectionType, resolveNextPageIndex 
-} from 'vueds/lib/store/'
-
-import * as keymage from '../keymage'
-
-export interface Opts {
-    flags: number
-    col_size: number
-    table_flags: number
-
-    loop_var: string
-    hammer: any,
-    pager: Pager
-    vm: any
-}
-
-var Hammer = require('hammerjs'),
-    current: Opts
-
-function moveUp(e) {
-    var target = e.target,
-        attr = target.getAttribute('c-list'),
-        pager,
-        clickUpdate: boolean
-    
-    if (attr && !(current = target.pager_opts)) {
-        target.pager_opts = current = resolveElement(target, attr).pager_opts
-    }
-    
-    if (!current || !(pager = current.pager).index_hidden) return
-    
-    clickUpdate = !!(current.flags & screen.flags)
-    if (current.col_size)
-        tableUp(pager, current.col_size, current.table_flags, pager.index_selected, e, clickUpdate)
-    else
-        listUp(pager, pager.index_selected, e, clickUpdate)
-}
-
-function moveDown(e) {
-    var target = e.target,
-        attr = target.getAttribute('c-list'),
-        pager: Pager,
-        clickUpdate: boolean
-    
-    if (attr && !(current = target.pager_opts)) {
-        target.pager_opts = current = resolveElement(target, attr).pager_opts
-    }
-    
-    if (!current || !(pager = current.pager).index_hidden) return
-    
-    clickUpdate = !!(current.flags & screen.flags)
-    if (current.col_size)
-        tableDown(pager, current.col_size, current.table_flags, pager.index_selected, e, clickUpdate)
-    else
-        listDown(pager, pager.index_selected, e, clickUpdate)
-}
-
-keymage.$('pager', 'up', moveUp)
-keymage.$('pager', 'down', moveDown)
-keymage.$('pager', 'defmod-up', (e) => { if (current) moveTopOrUp(e, current.pager, current) })
-keymage.$('pager', 'defmod-down', (e) => { if (current) moveBottomOrDown(e, current.pager, current) })
-keymage.$('pager', ['left', 'shift-up'], (e) => { if (current) moveLeft(e, current.pager, current) })
-keymage.$('pager', ['right', 'shift-down'], (e) => { if (current) moveRight(e, current.pager, current) })
-keymage.$('pager', ['defmod-left', 'defmod-shift-up'], (e) => { if (current) pageFirst(e, current.pager, current) })
-keymage.$('pager', ['defmod-right', 'defmod-shift-down'], (e) => { if (current) pageLast(e, current.pager, current) })
-keymage.$('pager', 'shift-space', (e) => { if (current) pageSort(e, current.pager, current) })
-keymage.$('pager', 'defmod-space', (e) => { if (current) pageReload(e, current.pager, current) })
-
-function focus(e, opts: Opts) {
-    current = opts
-    keymage.setScope('pager')
-}
-
-function swipe(e, opts: Opts) {
-    var target = e.target,
-        vfrag,
-        vm
-    if (isInput(target)) return
-    current = opts
-    keymage.setScope('pager')
-    
-    vfrag = vfragLookup(target)
-    if (!vfrag || !(vm = vfrag.scope) || !vm.hasOwnProperty('$index') || 
-        (vm !== opts.vm && vm.$parent !== opts.vm)) return
-    
-    switch(e.direction) {
-        case 2: // right-to-left
-            pageNextOrLoad(e, opts.pager, opts)
-            break
-        case 4: // left-to-right
-            pagePrevOrLoad(e, opts.pager, opts)
-            break;
-    }
-}
-
-function select(e, opts: Opts, dbltap: boolean, flagsIntersect: number) {
-    var target = e.target, 
-        parent = target.parentElement, 
-        vfrag = vfragLookup(target, parent),
-        pojo,
-        store: PojoStore<any>,
-        vm,
-        trigger = target.getAttribute('dtap') || (target=parent).getAttribute('dtap')
-    
-    if (trigger) fireEvent(target, 'dtap')
-    
-    if (!vfrag || !(vm = vfrag.scope) || !vm.hasOwnProperty('$index') || 
-        (vm !== opts.vm && vm.$parent !== opts.vm) || !(pojo = vm[opts.loop_var])) return
-    
-    store = opts.pager['store']
-
-    if (flagsIntersect) {
-        if (!(pojo.vstate & PojoState.UPDATE)) {
-            pojo.vstate |= PojoState.UPDATE
-            store.select(pojo, SelectionFlags.CLICKED_UPDATE, vm.$index)
-        }
-    } else if (dbltap && trigger) {
-        store.select(pojo, SelectionFlags.CLICKED, vm.$index)
-    } else {
-        pojo.vstate ^= PojoState.UPDATE
-        store.select(pojo, SelectionFlags.CLICKED_UPDATE, vm.$index)
-    }
-}
-
-function press(e, opts: Opts) {
-    current = opts
-    if (isInput(e.target)) return
-    keymage.setScope('pager')
-    
-    select(e, opts, false, opts.flags & screen.flags)
-}
-
-function tap(e, opts: Opts) {
-    current = opts
-    if (isInput(e.target)) return
-    keymage.setScope('pager')
-    
-    if (opts.flags & screen.flags) {
-        select(e, opts, false, 1)
-        return
-    }
-
-    var vfrag = vfragLookup(e.target),
-        pojo,
-        store: PojoStore<any>,
-        vm
-    if (!vfrag || !(vm = vfrag.scope) || !vm.hasOwnProperty('$index') || 
-        (vm !== opts.vm && vm.$parent !== opts.vm) || !(pojo = vm[opts.loop_var])) return
-    
-    store = opts.pager['store']
-    store.select(pojo, SelectionFlags.CLICKED, vm.$index)
-}
-
-function doubletap(e, opts: Opts) {
-    // TODO remove hack
-    if (e.target.hasOwnProperty('$l')) {
-        // a date input with a date picker
-        // workaround for mobile
-        fireEvent(e.target, 'dblclick')
-        e.preventDefault()
-        //e.stopPropagation()
-        return false
-    }
-    
-    current = opts
-    if (isInput(e.target)) return
-    // tap already handles this, which gets called before this function
-    // keymage.setScope('pager')
-    
-    select(e, opts, true, opts.flags & screen.flags)
-}
-/**
- * ```{loop_var}__{flags}__{table_flags?}```
- */
-function putArgsTo(opts: Opts, split_args: string[]) {
-    let i = 0, len = split_args.length
-    
-    opts.loop_var = split_args[i++]
-    opts.flags = parseInt(split_args[i++], 10)
-    
-    if (i === len) return
-    opts.table_flags = parseInt(split_args[i++], 10)
-    opts.col_size = table_compact_columns()
-}
-
-/*module.exports = {
-    flags: 0,
-    col_size: 0,
-    table_flags: 0,
-    pager: null,
-    */
 export function bind() {
     let el = this.el,
         arg: string = this.arg,
@@ -236,19 +31,10 @@ export function bind() {
     // TODO proper focus support
     el.addEventListener('focusin', opts.focus, true)
 
-    hammer.get('swipe').set({velocity: 0.1, distance: 1})
-    hammer.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) )
-    hammer.get('doubletap').recognizeWith('tap')
-
-    hammer.on('swipe', (e) => swipe(e, opts))
-    hammer.on('press', (e) => press(e, opts))
-    hammer.on('tap', (e) => tap(e, opts))
-    hammer.on('doubletap', (e) => doubletap(e, opts))
-
-    if (!el.id) return
-
-    el.addEventListener('page-left', (e) => { current = opts; pagePrevOrLoad(e, opts.pager, opts); })
-    el.addEventListener('page-right', (e) => { current = opts; pageNextOrLoad(e, opts.pager, opts); })
+    configureHammer(hammer, opts)
+    
+    if (el.id)
+        addCustomListenersTo(el, opts)
 }
 
 export function update(value: any, oldValue: any) {
@@ -260,4 +46,7 @@ export function unbind() {
     let opts = this.opts
     this.el.removeEventListener('focusin', opts.focus)
     opts.hammer.destroy()
+
+    if (opts.unbind)
+        opts.unbind()
 }
