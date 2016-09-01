3fdec40c85ba526036f192d32c6eb36f920e3469
dyu <david.yu.ftw@gmail.com>
2016-09-01 18:41:56 +0800

    generated files

--- lib/pager_util.d.ts
+++ lib/pager_util.d.ts
@@ -11,3 +11,20 @@ export declare function tableLeft(pager: Pager, col_size: number, flags: number,
 export declare function tableJumpLeft(pager: Pager, col_size: number, flags: number, idx: number, e: Event, clickUpdate: boolean): void;
 export declare function tableRight(pager: Pager, col_size: number, flags: number, idx: number, e: Event, clickUpdate: boolean): void;
 export declare function tableJumpRight(pager: Pager, col_size: number, flags: number, idx: number, e: Event, clickUpdate: boolean): void;
+export interface Opts {
+    col_size: number;
+    table_flags: number;
+    flags: number;
+}
+export declare function moveTopOrUp(e: any, pager: Pager, opts: Opts): void;
+export declare function moveBottomOrDown(e: any, pager: Pager, opts: Opts): void;
+export declare function pageFirst(e: any, pager: Pager, opts: Opts): void;
+export declare function pageLast(e: any, pager: Pager, opts: Opts): void;
+export declare function pageSort(e: any, pager: any, opts: Opts): void;
+export declare function pageNewer(e: any, pager: any, opts: Opts): void;
+export declare function pageOlder(e: any, pager: any, opts: Opts): void;
+export declare function pageReload(e: any, pager: any, opts: Opts): void;
+export declare function pagePrevOrLoad(e: any, pager: any, opts: Opts): void;
+export declare function pageNextOrLoad(e: any, pager: Pager, opts: Opts): void;
+export declare function moveLeft(e: any, pager: Pager, opts: Opts): void;
+export declare function moveRight(e: any, pager: Pager, opts: Opts): void;
