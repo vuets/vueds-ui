8c8c8283d06dcad2c2b819ba5c66c13195aed57e
dyu <david.yu.ftw@gmail.com>
2016-09-04 22:26:01 +0800

    move to common fn

--- src/d2/disable.ts
+++ src/d2/disable.ts
@@ -1,23 +1,8 @@
 import { VNode, VNodeDirective, VNodeWithData } from '../v2/'
 import { addClass, removeClass } from '../dom_util'
 
-export function bind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
-    /*let value = dir.value,
-        style = el.style
-
-    vnode = locateNode(vnode)
-    const transition = vnode.data && vnode.data.transition
-    if (value && transition && transition.appear && !isIE9) {
-      enter(vnode)
-    }
-    
-    const originalDisplay = style.display === 'none' ? '' : style.display
-    style.display = value ? originalDisplay : 'none'
-    el.__vOriginalDisplay = originalDisplay*/
-}
-
-export function update(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
-    let disabled = !!dir.value
+function doUpdate(el, value) {
+    let disabled = !!value
 
     // check the state before applying
     if (disabled === el.disabled)
@@ -26,6 +11,14 @@ export function update(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
     el.disabled = disabled
     if (disabled) addClass(el, 'disabled')
     else removeClass(el, 'disabled')
+}
+
+export function bind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    doUpdate(el, dir.value)
+}
+
+export function update(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    doUpdate(el, dir.value)
 }
 
 //export function unbind(el: any, dir: VNodeDirective, vnode: VNodeWithData) { }
