5e233ca24eb687f05c2cda8373030d6a1cb32914
dyu <david.yu.ftw@gmail.com>
2016-09-04 22:03:41 +0800

    generated files

--- lib/d2/defp.d.ts
+++ lib/d2/defp.d.ts
@@ -1,4 +1,2 @@
 import { VNodeDirective, VNodeWithData } from '../v2/';
 export declare function bind(el: any, dir: VNodeDirective, vnode: VNodeWithData): void;
-export declare function update(el: any, dir: VNodeDirective, vnode: VNodeWithData): void;
-export declare function unbind(el: any, dir: VNodeDirective, vnode: VNodeWithData): void;
