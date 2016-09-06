287ca28f3902843424adc383f1a28bce003302b4
dyu <david.yu.ftw@gmail.com>
2016-09-06 19:32:49 +0800

    generated files

---
+++ lib/_itoggle.d.ts
@@ -0,0 +1,14 @@
+export interface Opts {
+    type: string;
+    check_initial?: boolean;
+    class_def: string;
+    class_alt: string;
+    array: any[];
+    vm: any;
+    el: any;
+    handler: any;
+    index: number;
+    prevIndex: number | null;
+    el_icon: any;
+}
+export declare function parseOpts(args: string[], array: any[], vm: any, el: any): Opts;
