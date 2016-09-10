2b0311a7fb466be543ea9732a483c1423e6e4299
dyu <david.yu.ftw@gmail.com>
2016-09-10 11:54:39 +0800

    append directive

---
+++ src/d2/append.ts
@@ -0,0 +1,23 @@
+import { VNode, VNodeDirective, VNodeWithData } from '../v2/'
+import { defp } from 'vueds'
+import { resolveElement } from '../dom_util'
+
+export function bind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    if (!dir.arg) {
+        console.warn('arg is required for v-append.')
+        return
+    }
+
+    update(el, dir, vnode)
+}
+
+export function update(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    if (!dir.arg || !dir.value || dir.value === dir.oldValue) return
+
+    let el_append = el.append_
+    
+    if (!el_append)
+        defp(el, 'append_', el_append = resolveElement(el, dir.arg, vnode.context))
+    
+    el.appendChild(el_append)
+}
