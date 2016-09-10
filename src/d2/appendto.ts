973c9dd282d20b7ff3df4cf54ca012a9fac25833
dyu <david.yu.ftw@gmail.com>
2016-09-10 10:50:34 +0800

    appendto

---
+++ src/d2/appendto.ts
@@ -0,0 +1,25 @@
+import { VNode, VNodeDirective, VNodeWithData } from '../v2/'
+import { resolveElement } from '../dom_util'
+
+export function bind(el: any, dir: VNodeDirective, vnode: VNodeWithData) {
+    let value = dir.value,
+        vm = vnode.context,
+        target,
+        parent
+    
+    if (!Array.isArray(value)) {
+        target = el
+        parent = resolveElement(target, value, vm)
+    } else if (value.length === 1) {
+        target = el
+        parent = resolveElement(target, value[0], vm)
+    } else {
+        target = resolveElement(el, value[0], vm)
+        parent = resolveElement(target, value[1], vm)
+    }
+
+    if (dir.arg === '1')
+        parent.insertBefore(target, parent.lastElementChild)
+    else
+        parent.appendChild(target)
+}
