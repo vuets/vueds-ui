55be072377753863c76cee707be97e77e1fb667e
dyu <david.yu.ftw@gmail.com>
2016-09-14 01:25:11 +0800

    suggest impl partial

--- src/_suggest.ts
+++ src/_suggest.ts
@@ -3,48 +3,59 @@ import { SelectionType, SelectionFlags } from 'vueds/lib/store/'
 import { ds } from 'vueds/lib/ds/'
 import * as keymage from './keymage'
 import { getInstance } from './c/suggest'
-import { removeClass, addClass, hasClass, popTo, debounce } from './dom_util'
+import { removeClass, addClass, hasClass, positionTo, debounce } from './dom_util'
 import { listDown, listUp, moveTopOrUp, moveBottomOrDown } from './pager_util'
 
-function hidePopup(suggest, conditional?: boolean): boolean {
+function $show(popup, self: Opts) {
+    let style = popup.style
+    style.visibility = 'hidden'
+    addClass(popup, 'active')
+    positionTo(self.el, popup)
+    style.visibility = 'visible'
+}
+
+function $hide(popup) {
+    popup.style.visibility = 'hidden'
+    removeClass(popup, 'active')
+}
+
+function hideSuggest(suggest, conditional?: boolean): boolean {
     let popup = suggest.$el.parentElement
     
     if (conditional && !hasClass(popup, 'active'))
         return false
     
-    popup.style.visibility = 'hidden'
-    removeClass(popup, 'active')
-    
+    $hide(popup)
     return true
 }
-function showPopup(suggest, self: Opts) {
-    let popup = suggest.$el.parentElement
-    suggest.pstore.replace(self.cache, SelectionType.RESET)
 
-    addClass(popup, 'active')
-    popTo(self.el, popup)
-}
-function togglePopup(suggest, self: Opts): boolean {
+function showSuggest(suggest, self: Opts) {
     let popup = suggest.$el.parentElement,
-        show = true
+        style = popup.style
+    
+    suggest.pstore.replace(self.cache, SelectionType.RESET)
+    suggest.opts = self
+    $show(popup, self)
+}
+function toggleSuggest(suggest, self: Opts): boolean {
+    let popup = suggest.$el.parentElement,
+        show = true,
+        array
     
     if (hasClass(popup, 'active')) {
-        popup.style.visibility = 'hidden'
-        removeClass(popup, 'active')
+        $hide(popup)
         show = false
-    } else {
-        if (!suggest.pstore.isSameArray(self.cache))
-            suggest.pstore.replace(self.cache, SelectionType.RESET)
-        addClass(popup, 'active')
-        popTo(self.el, popup)
+    } else if ((array = self.cache).length) {
+        if (!suggest.pstore.isSameArray(array))
+            suggest.pstore.replace(array, SelectionType.RESET)
+        suggest.opts = self
+        $show(popup, self)
     }
     
     return show
 }
-function isPopupShown(suggest): boolean {
-    let popup = suggest.$el.parentElement
-    
-    return hasClass(popup, 'active')
+function isSuggestShown(suggest): boolean {
+    return hasClass(suggest.$el.parentElement, 'active')
 }
 
 export const enum Flags {
@@ -75,8 +86,12 @@ export interface Opts {
     unwatch: any
     onSelect: any
 
-    focusNT: any // next tick
-    focusin: any
+    // next tick
+    focusNT: any
+    hideSuggestNT: any
+
+    // listeners
+    //focusin: any
     focusout: any
     click: any
     input: any
@@ -84,7 +99,7 @@ export interface Opts {
 }
 
 const emptyArray = []
-var current: Opts, previous: Opts
+//var current: Opts, previous: Opts
 
 function newWatchFn(pojo, fk) {
     return function() {
@@ -112,13 +127,17 @@ function onSelect(message: ds.ACResult, flags: SelectionFlags) {
         self.pending_name = name
         self.pending_value = value
         self.el.value = name
+    } else if (name === self.pojo._[self.fk]) {
+        self.pending_name = null
+        self.el.value = name // redudant
+        addClass(this.el.parentElement, 'suggested')
+        Vue.nextTick(self.focusNT)
     } else {
         self.pending_name = null
         self.pojo._[self.fk] = name
         self.pojo[self.field] = value
+        Vue.nextTick(self.focusNT)
     }
-
-    Vue.nextTick(self.focusNT)
 }
 
 export function parseOpts(args: string[]|any, pojo, field, fetch, vm, el): Opts {
@@ -154,7 +173,8 @@ export function parseOpts(args: string[]|any, pojo, field, fetch, vm, el): Opts 
         onSelect: null,
 
         focusNT: null,
-        focusin: null,
+        hideSuggestNT: null,
+        //focusin: null,
         focusout: null,
         click: null,
         input: null,
@@ -164,12 +184,13 @@ export function parseOpts(args: string[]|any, pojo, field, fetch, vm, el): Opts 
     opts.unwatch = vm.$watch(newWatchFn(pojo, fk), onUpdate.bind(opts))
     opts.onSelect = onSelect.bind(opts)
     opts.focusNT = focusNT.bind(opts)
+    opts.hideSuggestNT = hideSuggestNT.bind(opts)
 
-    el.addEventListener('focusin', opts.focusin = focusin.bind(opts))
+    //el.addEventListener('focusin', opts.focusin = focusin.bind(opts))
     el.addEventListener('focusout', opts.focusout = focusout.bind(opts))
     //el.addEventListener('focusout', opts.focusout = debounce(focusout.bind(opts), 200))
     el.addEventListener('click', opts.click = click.bind(opts))
-    el.addEventListener('input', opts.input = input.bind(opts))
+    el.addEventListener('input', opts.input = debounce(input.bind(opts), 250))
     el.addEventListener('keyup', opts.keyup = keyup.bind(opts))
 
     return opts
@@ -177,7 +198,7 @@ export function parseOpts(args: string[]|any, pojo, field, fetch, vm, el): Opts 
 
 export function cleanup(opts: Opts) {
     let el = opts.el
-    el.removeEventListener('focusin', opts.focusin)
+    //el.removeEventListener('focusin', opts.focusin)
     el.removeEventListener('focusout', opts.focusout)
     el.removeEventListener('click', opts.click)
     el.removeEventListener('input', opts.input)
@@ -185,7 +206,7 @@ export function cleanup(opts: Opts) {
     opts.unwatch()
 }
 
-function focusin(e) {
+/*function focusin(e) {
     e.preventDefault()
     e.stopPropagation()
 
@@ -194,23 +215,46 @@ function focusin(e) {
 
     //if (previous !== current)
     //    hidePopup(getInstance(), true)
+}*/
+
+function hideSuggestNT() {
+    let suggest = getInstance()
+    if (this === suggest.opts && hideSuggest(suggest, true)) {
+        // hidden
+    }
 }
 
 function focusout(e) {
     let self: Opts = this,
-        name = self.pending_name
+        name = self.pending_name,
+        text = self.el.value,
+        suggest = getInstance()
     
     if (name) {
-        if (self.el.value === name) {
-            self.pojo._[self.fk] = self.pending_name
+        if (!self.update && text !== name) {
+            removeClass(self.el.parentElement, 'suggested')
+        } else if (name === self.pojo._[self.fk]) {
+            self.el.value = name // redudant on non update
+            addClass(self.el.parentElement, 'suggested')
+        } else {
+            self.pojo._[self.fk] = name
             self.pojo[self.field] = self.pending_value
         }
         self.pending_name = null
-    } else if (self.update && self.el.value !== (name = self.pojo._[self.fk])) {
+    } else if (text === (name = self.pojo._[self.fk])) {
+        addClass(self.el.parentElement, 'suggested') // redudant
+    } else if (self.update) {
         self.el.value = name
+        addClass(self.el.parentElement, 'suggested')
+    } else if (text) {
+        removeClass(self.el.parentElement, 'suggested')
+    } else if (name) {
+        self.el.value = name
+        addClass(self.el.parentElement, 'suggested')
     }
 
-    hidePopup(getInstance(), true)
+    if (self === suggest.opts)
+        window.setTimeout(self.hideSuggestNT, 100)
 }
 
 function click(e) {
@@ -220,14 +264,14 @@ function click(e) {
         self: Opts,
         text: string
 
-    if (isPopupShown(suggest))
+    if (isSuggestShown(suggest))
         return
     
     self = this
     text = self.el.value
     
     if (text && text === self.str && self.cache.length) {
-        showPopup(suggest, self)
+        showSuggest(suggest, self)
     }
 }
 
@@ -237,41 +281,49 @@ function input(e) {
         value: string = el.value.trim()
     
     if (self.disabled) {
-        // just to make sure
+        // ignore
     } else if (value.length !== el.value.length) {
         // the new input has whitespace, replace with trimmed string
         el.value = value
     } else if (!value) {
         self.empty = true
-        hidePopup(getInstance())
+        hideSuggest(getInstance())
     } else if (value === self.str) {
         // simply re-typed the single letter char
-        showPopup(getInstance(), self)
+        showSuggest(getInstance(), self)
     } else {
-        el.disabled = self.disabled = true
-        removeClass(self.el.parentElement, 'suggested')
+        self.disabled = true
         self.fetch(ds.ParamRangeKey.$create(false, 11), value)
             .then(data => {
+                self.disabled = false
+
+                if (value !== self.el.value) {
+                    Vue.nextTick(self.input)
+                    return true
+                }
+
                 let array = data['1'],
-                    empty = !array || !array.length,
                     suggest = getInstance()
                 
                 suggest.opts = self
                 self.str = value
-                if (empty) {
+                if (!array || !array.length) {
                     self.cache = emptyArray
+                    hideSuggest(suggest, true)
                 } else {
                     self.cache = array.reverse()
-                    showPopup(suggest, self)
+                    showSuggest(suggest, self)
                     Vue.nextTick(self.focusNT)
                 }
 
-                el.disabled = self.disabled = false
-                return empty
+                return true
             })
             .then(undefined, err => {
                 // ignore
-                el.disabled = self.disabled = false
+                self.disabled = false
+                if (value !== self.el.value) {
+                    Vue.nextTick(self.input)
+                }
             })
     }
 }
@@ -285,7 +337,7 @@ function keyup(e) {
         //    return self._input(e)
         case 13:
             // do not propagate the enter key event
-            if (!togglePopup(getInstance(), self) && self.el.value === self.pending_name) {
+            if (!toggleSuggest(getInstance(), self) && self.el.value === self.pending_name) {
                 self.pojo._[self.fk] = self.pending_name
                 self.pojo[self.field] = self.pending_value
                 self.pending_name = null
@@ -301,7 +353,7 @@ function keyup(e) {
                 getOwner(self).vmessage['f'+self.field_key] = false
             }*/
             //self.pending_name = null
-            hidePopup(getInstance(), true)
+            hideSuggest(getInstance(), true)
             break
         /*case 37: // left
             if (!util.isPopupShown()) return true
@@ -310,6 +362,8 @@ function keyup(e) {
             break*/
         case 38: // up
             suggest = getInstance()
+            if (!isSuggestShown(suggest)) break
+
             pager = suggest.pager
             if (e.ctrlKey) moveTopOrUp(e, pager, self)
             else listUp(pager, pager.index_selected, e, false)
@@ -321,6 +375,8 @@ function keyup(e) {
             break*/
         case 40: // down
             suggest = getInstance()
+            if (!isSuggestShown(suggest)) break
+
             pager = suggest.pager
             if (e.ctrlKey) moveBottomOrDown(e, pager, self)
             else listDown(pager, pager.index_selected, e, false)
