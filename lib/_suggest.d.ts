c8316e76912dd652a49e8e7cb33fa67961eb2c3f
dyu <david.yu.ftw@gmail.com>
2016-09-14 01:25:23 +0800

    generated files

--- lib/_suggest.d.ts
+++ lib/_suggest.d.ts
@@ -1,3 +1,6 @@
+export declare const enum Flags {
+    UPDATE = 16,
+}
 export interface Opts {
     flags: number;
     pojo: any;
@@ -6,16 +9,23 @@ export interface Opts {
     fk: string;
     vm: any;
     el: any;
+    col_size: number;
+    table_flags: number;
+    update: boolean;
     str: string;
     empty: boolean;
     disabled: boolean;
     cache: any;
+    pending_name: any;
+    pending_value: any;
     unwatch: any;
     onSelect: any;
-    focusin: any;
+    focusNT: any;
+    hideSuggestNT: any;
     focusout: any;
     click: any;
     input: any;
+    keyup: any;
 }
 export declare function parseOpts(args: string[] | any, pojo: any, field: any, fetch: any, vm: any, el: any): Opts;
 export declare function cleanup(opts: Opts): void;
