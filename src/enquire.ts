21b73b6386e5145e39ca9f885e24c394c514c3da
dyu <david.yu.ftw@gmail.com>
2016-09-24 14:28:36 +0800

    cleanup

--- src/enquire.ts
+++ src/enquire.ts
@@ -31,7 +31,7 @@ interface QueryHandlerOpts {
     deferSetup?: boolean
 }
 
-class QueryHandler2 {
+class QueryHandler {
     initialized = false
 
     constructor(private options: any) {
@@ -63,85 +63,8 @@ class QueryHandler2 {
     }
 }
 
-/**
- * Delegate to handle a media query being matched and unmatched.
- *
- * @param {object} options
- * @param {function} options.match callback for when the media query is matched
- * @param {function} [options.unmatch] callback for when the media query is unmatched
- * @param {function} [options.setup] one-time callback triggered the first time a query is matched
- * @param {boolean} [options.deferSetup=false] should the setup callback be run immediately, rather than first time query is matched?
- * @constructor
- */
-function QueryHandler(options) {
-    this.options = options;
-    !options.deferSetup && this.setup();
-}
-QueryHandler.prototype = {
-
-    /**
-     * coordinates setup of the handler
-     *
-     * @function
-     */
-    setup: function () {
-        if (this.options.setup)
-            this.options.setup()
-        
-        this.initialized = true
-    },
-
-    /**
-     * coordinates setup and triggering of the handler
-     *
-     * @function
-     */
-    on: function () {
-        !this.initialized && this.setup()
-        this.options.match && this.options.match()
-    },
-
-    /**
-     * coordinates the unmatch event for the handler
-     *
-     * @function
-     */
-    off: function () {
-        this.options.unmatch && this.options.unmatch()
-    },
-
-    /**
-     * called when a handler is to be destroyed.
-     * delegates to the destroy or unmatch callbacks, depending on availability.
-     *
-     * @function
-     */
-    destroy: function () {
-        this.options.destroy ? this.options.destroy() : this.off()
-    },
-
-    /**
-     * determines equality by reference.
-     * if object is supplied compare options, if function, compare match callback
-     *
-     * @function
-     * @param {object || function} [target] the target for comparison
-     */
-    equals: function (target) {
-        return this.options === target || this.options.match === target
-    }
-
-};
-
 function mqListener(this: MediaQuery, mql: MediaQueryList) {
     this.mql = mql
-}
-
-function cbRemoveHandler(this: MediaQuery, h, i) {
-    if (h.equals(this.rm_handler)) {
-        h.destroy()
-        return !this.handlers.splice(i, 1) //remove from array and exit each early
-    }
 }
 
 function cbClear(handler) {
@@ -160,15 +83,13 @@ function cbOff(handler) {
  * Represents a single media query, manages it's state and registered handlers for this query
  */
 class MediaQuery {
-    handlers: QueryHandler2[] = []
+    handlers: QueryHandler[] = []
     mql: MediaQueryList
     listener: any
-    rm_handler: any
     cbRemoveHandler: any
     constructor(private query: string, private isUnconditional: boolean) {
         this.mql = matchMedia(query)
         this.listener = mqListener.bind(this)
-        this.cbRemoveHandler = mqListener.bind(this)
     }
 
     matches(): boolean {
@@ -183,8 +104,19 @@ class MediaQuery {
     }
 
     removeHandler(handler) {
-        this.rm_handler = handler
-        each(this.handlers, cbRemoveHandler)
+        let handlers = this.handlers,
+            len = handlers.length,
+            i = 0,
+            h
+        for (; i < len; i++) {
+            h = handlers[i]
+            if (!h.equals(handler))
+                continue
+            
+            h.destroy()
+            handlers.splice(i, 1)
+            break
+        }
     }
 
     clear() {
@@ -198,16 +130,21 @@ class MediaQuery {
     }
 }
 
-export interface MediaMatchOpts {
+interface MediaQueryMap {
+    [index: string]: MediaQuery|null
+}
+
+export interface Opts {
     match: Function
+    unmatch: Function
 }
 
 /**
  * Registers a handler for the given media query
  */
-export class MediaQueryDispatch2 {
-    queries = {}
-    browserIsIncapable: boolean
+export class MediaQueryDispatch {
+    private queries: MediaQueryMap = {}
+    private browserIsIncapable: boolean
     constructor() {
         if (!matchMedia)
             throw new Error('matchMedia not present, legacy browsers require a polyfill')
@@ -215,21 +152,19 @@ export class MediaQueryDispatch2 {
         this.browserIsIncapable = !matchMedia('only all').matches
     }
 
-    register(q: string, options: MediaMatchOpts, shouldDegrade: boolean) {
-        var queries = this.queries,
-            isUnconditional = shouldDegrade && this.browserIsIncapable,
+    register(q: string, options: Opts, shouldDegrade?: boolean) {
+        let queries = this.queries,
             query = queries[q]
-
+        
         if (!query)
-            queries[q] = query = new MediaQuery(q, isUnconditional)
+            queries[q] = query = new MediaQuery(q, !!shouldDegrade && this.browserIsIncapable)
         
         query.addHandler(options)
-
         return this
     }
     
     unregister(q: string, handler: any) {
-        var query: MediaQuery = this.queries[q]
+        const query = this.queries[q]
         if (!query) return this
 
         if (handler) {
@@ -244,4 +179,4 @@ export class MediaQueryDispatch2 {
     }
 }
 
-export default new MediaQueryDispatch2()
+export default new MediaQueryDispatch()
