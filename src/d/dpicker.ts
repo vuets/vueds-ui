36e36b5d8fdfe822c1226b762dde3ddb8bf722ed
dyu <david.yu.ftw@gmail.com>
2016-09-29 16:00:03 +0800

    naming convention

--- src/d/dpicker.ts
+++ src/d/dpicker.ts
@@ -4,13 +4,13 @@ export function update(value: any, oldValue: any) {
     if (oldValue) return
 
     if (value && value.pojo && value.field)
-        this.calendar = parseOpts(this.arg && this.arg.split(','), value.pojo, value.field, this.el)
+        this.dpicker = parseOpts(this.arg && this.arg.split(','), value.pojo, value.field, this.el)
     else
         console.warn('Must provide value: { pojo: obj, field: string }')
 }
 
 export function unbind() {
-    let calendar = this.calendar
-    if (calendar)
-        cleanup(calendar)
+    let dpicker = this.dpicker
+    if (dpicker)
+        cleanup(dpicker)
 }
