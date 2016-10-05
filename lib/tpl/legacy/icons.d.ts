4d169f3bfc55df5fcf33109de02ed5a0f073aabf
dyu <david.yu.ftw@gmail.com>
2016-10-05 12:40:50 +0800

    generated files

--- lib/tpl/legacy/icons.d.ts
+++ lib/tpl/legacy/icons.d.ts
@@ -1,7 +1,9 @@
 export interface ToggleOpts {
     pojo: string;
+    field: string;
     fn: string;
     bit?: number;
     icon_class?: string;
+    title_expr?: string;
 }
 export declare function toggle(it: ToggleOpts): string;
