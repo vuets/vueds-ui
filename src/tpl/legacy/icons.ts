045a8b6d6367834ebb89f25ff6e4d512e4b497a2
dyu <david.yu.ftw@gmail.com>
2016-10-05 12:40:47 +0800

    title_expr

--- src/tpl/legacy/icons.ts
+++ src/tpl/legacy/icons.ts
@@ -1,15 +1,18 @@
 export interface ToggleOpts {
     pojo: string
+    field: string
     fn: string
     bit?: number
-    icon_class?: string 
+    icon_class?: string
+    title_expr?: string
 }
 
 export function toggle(it: ToggleOpts): string {
     let pojo = it.pojo,
-        bit = it.bit || 1
+        bit = it.bit || 1,
+        title_expr = !it.title_expr ? '' : (` :title="${it.title_expr}"`)
     return `
-<i class="icon ${it.icon_class || 'circle'}" v-sclass:empty="!${pojo}.active" title="Active" @click="${pojo}._.vstate |= ${bit}"></i>
+<i class="icon ${it.icon_class || 'circle'}" v-sclass:empty="!${pojo}.${it.field}" @click="${pojo}._.vstate |= ${bit}"${title_expr}></i>
 <i class="icon ok-circled" v-show="(${pojo}._.vstate & ${bit})" @click="${it.fn}(${pojo}, ${pojo}._.vstate ^= ${bit})"></i>
 <i class="icon cancel-circled" v-show="(${pojo}._.vstate & ${bit})" @click="${pojo}._.vstate ^= ${bit}"></i>`
 }
