6436f486d18bc3a3d1fd398d3444b9492a65055e
dyu <david.yu.ftw@gmail.com>
2016-10-06 18:10:08 +0800

    checkbox bitset

---
+++ src/bit_util.ts
@@ -0,0 +1,46 @@
+
+export interface Entry {
+    display: string
+    value: number
+    checked: boolean
+}
+
+export interface Container {
+    show: boolean
+    [propName: string]: Entry|any
+}
+
+export function putCheckboxBitset(container1: Container, container2: Container,
+        bitArray1: Entry[], bitArray2: Entry[], 
+        arrayValue: number[], arrayDisplay: string[]) {
+    var display: string,
+        value: number,
+        strIdx: string,
+        entry: Entry
+    
+    container2.show = false
+    
+    for (var i = 0, len = arrayValue.length; i < len; i++) {
+        display = arrayDisplay[i]
+        value = arrayValue[i]
+        strIdx = '$' + value
+
+        // 1
+        entry = {
+            display,
+            value,
+            checked: false
+        }
+        bitArray1.push(entry)
+        container1[strIdx] = entry
+
+        // 2
+        entry = {
+            display,
+            value,
+            checked: false
+        }
+        bitArray2.push(entry)
+        container2[strIdx] = entry
+    }
+}
