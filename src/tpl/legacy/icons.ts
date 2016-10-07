91d05cd6c5880cd98fef61bedc44ce2903daa229
dyu <david.yu.ftw@gmail.com>
2016-10-08 01:03:59 +0800

    wrap click handlers in parens

--- src/tpl/legacy/icons.ts
+++ src/tpl/legacy/icons.ts
@@ -22,9 +22,9 @@ export function toggle(it: ToggleOpts): string {
         bit = it.bit,
         title_expr = !it.title_expr ? '' : (` :title="${it.title_expr}"`)
     return `
-<i class="icon ${it.icon_class || 'circle'}" v-sclass:empty="!${pojo}.${it.field}" @click="${pojo}._.vstate |= ${bit}"${title_expr}></i>
+<i class="icon ${it.icon_class || 'circle'}" v-sclass:empty="!${pojo}.${it.field}" @click="(${pojo}._.vstate |= ${bit})"${title_expr}></i>
 <i class="icon ok-circled" v-show="(${pojo}._.vstate & ${bit})" @click="${it.fn}(${pojo}, ${pojo}._.vstate ^= ${bit})"></i>
-<i class="icon cancel-circled" v-show="(${pojo}._.vstate & ${bit})" @click="${pojo}._.vstate ^= ${bit}"></i>`
+<i class="icon cancel-circled" v-show="(${pojo}._.vstate & ${bit})" @click="(${pojo}._.vstate ^= ${bit})"></i>`
 }
 
 export interface DrawerOpts extends CommonOpts {
