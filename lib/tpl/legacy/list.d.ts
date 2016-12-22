151c8e427a84afcf1827d9635b2f9f162b833b3e
dyu <david.yu.ftw@gmail.com>
2016-12-22 12:12:39 +0800

    generated files

--- lib/tpl/legacy/list.d.ts
+++ lib/tpl/legacy/list.d.ts
@@ -10,4 +10,16 @@ export interface Opts {
     item_class_exprs?: any;
     pojo?: string;
 }
+export declare function item(it: Opts, content: string, initialAttrs?: string): string;
+export declare function new_pi(it: Opts): {
+    name: string;
+    props: {
+        pojo: {
+            type: ObjectConstructor;
+            required: boolean;
+        };
+    };
+    template: string;
+};
+export declare function pi(pager: string, content: string, custom_list_class?: string, ul_attrs?: any): string;
 export declare function main(it: Opts, content: string): string;
