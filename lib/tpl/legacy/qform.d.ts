fbe139cb25a2a21d78d5c24f33d9f3af0d4f8d4a
dyu <david.yu.ftw@gmail.com>
2017-01-01 15:10:58 +0800

    generated files

--- lib/tpl/legacy/qform.d.ts
+++ lib/tpl/legacy/qform.d.ts
@@ -2,12 +2,12 @@ export interface Opts {
     qd: any;
     pager?: string;
 }
-export declare function field_enum(it: Opts, fd: any, pojo: string, display: string): string;
-export declare function field_bool(it: Opts, fd: any, pojo: string, display: string): string;
-export declare function field_suggest(it: Opts, fd: any, pojo: string, display: string): string;
-export declare function field_num(it: Opts, fd: any, pojo: string, display: string): string;
-export declare function field_num_range(it: Opts, fd: any, pojo: string, display: string): string;
-export declare function field_default(it: Opts, fd: any, pojo: string, display: string, changeSuffix: string): string;
+export declare function field_enum(fd: any, pojo: string, display: string): string;
+export declare function field_bool(fd: any, pojo: string, display: string): string;
+export declare function field_suggest(fd: any, pojo: string, display: string): string;
+export declare function field_num(fd: any, pojo: string, display: string): string;
+export declare function field_num_range(fd: any, pojo: string, display: string): string;
+export declare function field_default(fd: any, pojo: string, display: string, changeSuffix: string): string;
 export declare function filter_fields(it: Opts, jso: any, fields: number[], pojo: string, nf: string): string;
 export declare function items(it: Opts, values: any[]): string;
 export declare function main(it: Opts): string;
