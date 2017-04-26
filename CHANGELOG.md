e6278510be1c9960c4e97e742fe0e658ad712a6b
dyu <david.yu.ftw@gmail.com>
2017-04-26 10:30:45 +0800

    action class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.7.2
+
+* add "action" class to toggle/drawer icons
+
 ## 0.7.1 (2017-04-24)
 
 * Fix peer dependency version on vueds
--- src/tpl/legacy/icons.ts
+++ src/tpl/legacy/icons.ts
@@ -36,9 +36,9 @@ export function toggle(it: ToggleOpts): string {
         bit = it.bit,
         title_expr = !it.title_expr ? '' : (` :title="${it.title_expr}"`)
     return /**/`
-<i class="icon ${it.icon_class || 'circle'}" v-sclass:empty="!${pojo}.${it.field}" @click="(${pojo}._.state ^= ${bit})"${title_expr}></i>
-<i class="icon ok-circled" v-show="(${pojo}._.state & ${bit})" @click="${it.fn}(${pojo}, '${it.field}', ${pojo}._.state ^= ${bit})"></i>
-<i class="icon cancel-circled" v-show="(${pojo}._.state & ${bit})" @click="(${pojo}._.state ^= ${bit})"></i>
+<i class="icon action ${it.icon_class || 'circle'}" v-sclass:empty="!${pojo}.${it.field}" @click="(${pojo}._.state ^= ${bit})"${title_expr}></i>
+<i class="icon action ok-circled" v-show="(${pojo}._.state & ${bit})" @click="${it.fn}(${pojo}, '${it.field}', ${pojo}._.state ^= ${bit})"></i>
+<i class="icon action cancel-circled" v-show="(${pojo}._.state & ${bit})" @click="(${pojo}._.state ^= ${bit})"></i>
 `/**/
 }
 
@@ -66,7 +66,7 @@ export function drawer(it: DrawerOpts, content?: string): string {
 
     return /**/`
 <div class="content" @click="${call_expr}">
-  <i ${tabindex}class="icon" v-pclass:angle-="(${pojo}._.state & ${bit}) ? 'down' : 'right'"></i>${content || ''}
+  <i ${tabindex}class="icon action" v-pclass:angle-="(${pojo}._.state & ${bit}) ? 'down' : 'right'"></i>${content || ''}
 </div>
 <dd v-show="(${pojo}._.state & ${bit})"${append}></dd>
 `/**/
